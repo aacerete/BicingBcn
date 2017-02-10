0ac0bb33103da52bc144b448314b744f08bef500
aacerete <Deltous1>
2017-02-10 15:49:36 +0100

    Mapview inizialitzat

--- app/src/main/java/com/example/a46990527d/bicingbcn/MainActivityFragment.java
+++ app/src/main/java/com/example/a46990527d/bicingbcn/MainActivityFragment.java
@@ -6,10 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private org.osmdroid.views.MapView mvMap;
 
     public MainActivityFragment() {
     }
@@ -17,6 +22,23 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+        mvMap = (org.osmdroid.views.MapView) view.findViewById(R.id.mvMap);
+
+        initializeMap();
+
+        return view;
+
     }
+
+    private void initializeMap() {
+
+        mvMap.setTileSource(TileSourceFactory.MAPNIK);
+        mvMap.setTilesScaledToDpi(true);
+        mvMap.setBuiltInZoomControls(true);
+        mvMap.setMultiTouchControls(true);
+
+    }
+
 }
