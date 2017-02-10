bc2edeaf8c0dbf33b9b992e9f0ffb2bca93d3b72
aacerete <Deltous1>
2017-02-10 16:08:02 +0100

    Afegits overlays

--- app/src/main/java/com/example/a46990527d/bicingbcn/MainActivityFragment.java
+++ app/src/main/java/com/example/a46990527d/bicingbcn/MainActivityFragment.java
@@ -1,20 +1,40 @@
 package com.example.a46990527d.bicingbcn;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.*;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    private CridaApi cridaApi = new CridaApi();
     private org.osmdroid.views.MapView mvMap;
+
+    private IMapController mapController;
+    private MyLocationNewOverlay myLocationOverlay;
+    private MinimapOverlay mMinimapOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;
+    private CompassOverlay mCompassOverlay;
 
     public MainActivityFragment() {
     }
@@ -39,6 +59,81 @@ public class MainActivityFragment extends Fragment {
         mvMap.setBuiltInZoomControls(true);
         mvMap.setMultiTouchControls(true);
 
+    }
+
+    private void setZoom() {
+        mapController = mvMap.getController();
+        mapController.setZoom(14);
+    }
+
+    private void setOverlays() {
+        final DisplayMetrics dm = getResources().getDisplayMetrics();
+        myLocationOverlay = new MyLocationNewOverlay(
+                new GpsMyLocationProvider(getContext()),
+                mvMap
+        );
+
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.runOnFirstFix(new Runnable() {
+            @Override
+            public void run() {
+                mapController.animateTo(myLocationOverlay.getMyLocation());
+            }
+        });
+
+        mScaleBarOverlay = new ScaleBarOverlay(mvMap);
+        mScaleBarOverlay.setCentred(true);
+        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+
+        mCompassOverlay = new CompassOverlay(
+                getContext(),
+                new InternalCompassOrientationProvider(getContext()),
+                mvMap
+        );
+
+        mCompassOverlay.enableCompass();
+
+        mvMap.getOverlays().add(myLocationOverlay);
+        mvMap.getOverlays().add(mMinimapOverlay);
+        mvMap.getOverlays().add(mScaleBarOverlay);
+        mvMap.getOverlays().add(mCompassOverlay);
+
+
+    }
+
+
+    private void refresh(){
+
+        RefreshDataTask task = new RefreshDataTask();
+        task.execute();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        refresh();
+    }
+
+    private class RefreshDataTask extends AsyncTask<Void, Void, ArrayList<Station>> {
+
+        @Override
+        protected ArrayList<Station> doInBackground(Void... voids) {
+
+            ArrayList<Station> result;
+            result = cridaApi.getBicing();
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Station> stations) {
+
+            for (Station station : stations){
+
+                Log.d("Carrer", station.getStreetName());
+            }
+
+        }
     }
 
 }
